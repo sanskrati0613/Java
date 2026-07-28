f5302e2d0a797346bb0972b6e2e43550cabd5628
Sanskati Jain <sanskratijain88@gmail.com>
2026-07-28 21:44:30 +0530

    First Day

---
+++ src/DataTypes.java
@@ -0,0 +1,48 @@
+
+public class DataTypes {
+	
+	public static void main(String[] args) {
+		
+		//Numeric DT - short, byte, int, long
+		
+		byte num1 = 127;
+		//byte num2 = 500;	//Out of range
+		System.out.println(num1);
+		//System.out.println(num2);
+		
+		short num2 = 32767;
+		System.out.println(num2);
+		
+		int num3 = 50000;
+		System.out.println(num3);
+		long num4 = 736483757;
+		System.out.println(num4);
+		
+		//Floating DT
+		float num5 = 3.14f;
+		System.out.println(num5);
+		
+		double num6 = 3.14123913;
+		System.out.println(num6);
+		
+		// Char, Boolean
+		boolean eligibleToVote = true;
+		System.out.println(eligibleToVote);
+		
+		char ch = 'a';
+		System.out.println("My first character is " + ch);
+		System.out.println("My first character is " + (char)(ch+2) );
+		
+		//Implicit type conversion
+		byte num7 = 127;
+		long newNum7 = num7;
+		System.out.println(newNum7);
+		
+		//Explicit type conversion
+		long value1 = 123456778;
+		int value2 =(int)value1;
+		System.out.println(value2);
+		
+	}
+	
+}
---
+++ src/Variable.java
@@ -0,0 +1,51 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		//create //declare
+		int age;
+		//assign value
+		age=10;
+		age=11; 
+		System.out.println(age);
+		
+		//define or initialize
+		int totalMarks = 120;
+		System.out.println(totalMarks);
+		
+		//Case Sensitive
+		int weight = 80;
+		int WEIGHT = 90;
+		System.out.println(weight);
+		System.out.println(WEIGHT);
+
+		//Starting letter
+		int marks = 20;
+		int Marks = 30;
+		int _marks = 40;
+		int $marks = 50;
+		
+		//Subsequent letters
+		int height5 = 21;
+		int height_ = 21;
+		int height$ = 21;
+		
+		//No reserved Keywords
+		//int class = 22;
+		int main = 22;	//main is not a reserved keyword
+		System.out.println(main);
+		
+		//camelCase
+		int myVariable = 19;
+		System.out.println(myVariable);
+		
+		//constants
+		int DAYS_IN_YEAR = 365;
+		
+		
+		
+		
+		
+	}
+
+}
